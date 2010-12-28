bdd6ef83b7febc95f075048ab98c3f6aa66ca598
Nishibayashi Takuji <takuji@senchan.jp>
2010-12-28 16:52:44 +0900

    スライド追加のページ追加（形だけ）

---
+++ lib/ThreeS/Web/C/Slide.pm
@@ -0,0 +1,14 @@
+package  ThreeS::Web::C::Slide;
+use Chiffon::Core;
+use Chiffon::Web::Controller;
+use ThreeS::Container;
+
+sub do_index {
+    my ( $class, $c ) = @_;
+}
+sub do_create {
+    my ( $class, $c ) = @_;
+}
+
+1;
+
