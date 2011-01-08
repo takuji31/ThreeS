8357bb39cdba47c88b09d14490c5268a3a606e21
Nishibayashi Takuji <takuji@senchan.jp>
2011-01-08 11:42:42 +0900

    テンプレートのマクロを追加、テンプレートの構成も変更

--- config.pl
+++ config.pl
@@ -5,24 +5,24 @@ use Path::Class;
 my $home = container('home');
 return +{
     common => {
-        app_name  => 'three_s',
-        title     => 'ThreeS - The web slide system',
-        copyright => '2010 Senchan all rights reserved',
+        app_name    => 'three_s',
+        title       => 'ThreeS - The web slide system',
+        copyright   => '2010 Senchan all rights reserved',
         global_navi => [
             {
-                title => 'Top',
-                link  => '/',
-                name  => 'top'
+                title      => 'Top',
+                link       => '/',
+                controller => 'Root'
             },
             {
-                title => 'SlideShow',
-                link  => '/slideshow/',
-                name  => 'slideshow'
+                title      => 'SlideShow',
+                link       => '/slide/',
+                controller => 'Slide'
             },
         ],
         view => {
             'Chiffon::View::Xslate' => +{
-                path   => $home->file('assets/template')->stringify,
+                path      => $home->file('assets/template')->stringify,
                 cache     => 1,
                 cache_dir => '/tmp/three_s',
                 syntax    => 'Kolon',
@@ -33,27 +33,24 @@ return +{
         datasource => +{
             master => +{
                 dsn => 'dbi:mysql:three_s;user=root',
-            },
+            }, 
         },
-        hostname => +{
-        },
-        plugins => +{
-        },
+        hostname => +{},
+        plugins  => +{},
     },
-    dev     => {
+    dev => {
         datasource => +{
             master => +{
-                dsn => 'dbi:mysql:study_dev;user=root',
+                dsn => 'dbi:mysql:study_dev;user=root', 
             },
         },
     },
     production => {
         datasource => +{
-            master => +{
-                dsn => 'dbi:mysql:three_s;user=root',
+                master => +{
+                    dsn => 'dbi:mysql:three_s;user=root',
+                },
             },
-        },
     },
 };
-
 
